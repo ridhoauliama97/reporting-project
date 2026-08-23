aa3837e3223c140d845577c980d13a712b71a651
ridhoauliama97 <ridhoauliama97@gmail.com>
2026-08-23 15:54:06 +0700

    fix: type handler with express request/response

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,10 +1,8 @@
+import type { Request, Response } from "express";
 import { createApp } from "../src/app.js";
 
 const app = createApp();
 
-export default function handler(
-  req: Parameters<typeof app.handle>[0],
-  res: Parameters<typeof app.handle>[1],
-) {
+export default function handler(req: Request, res: Response) {
   app(req, res);
 }
